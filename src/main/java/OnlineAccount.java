58af372e803b9a531626e41bce0b401768a66634
AvianLearning <iaradcliffe@hotmail.com>
2020-11-24 20:41:00 +0000

    Added OnlineAccount class and tested can get name, add payment method and add transaction fee

--- src/main/java/OnlineAccount.java
+++ src/main/java/OnlineAccount.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 public class OnlineAccount {
     private String name;
-    private ArrayList<PaymentCard> paymentMethods;
+    private ArrayList<IChargeable> paymentMethods;
     private ArrayList<Double> transactionFees;
 
     public OnlineAccount(String name) {
@@ -15,7 +15,7 @@ public class OnlineAccount {
         return name;
     }
 
-    public ArrayList<PaymentCard> getPaymentMethods() {
+    public ArrayList<IChargeable> getPaymentMethods() {
         return paymentMethods;
     }
 
@@ -23,4 +23,19 @@ public class OnlineAccount {
         return transactionFees;
     }
 
+    public void addPaymentMethod(IChargeable card) {
+        this.paymentMethods.add(card);
+    }
+
+    public void addTransactionFee(Double fee) {
+        this.transactionFees.add(fee);
+    }
+
+    public void chargeCustomer(int paymentMethodIndex, double transactionCost) {
+        IChargeable cardToCharge = this.paymentMethods.get(paymentMethodIndex);
+        cardToCharge.charge(transactionCost);
+        Double transactionFee = cardToCharge.getTransactionFee(transactionCost);
+//    Should be able to chargeCustomer(int paymentMethodIndex, double transactionCost) via a selected paymentMethod:
+//    charging that payment method (.charge(cost)) and logging the transaction fee in the OnlineAccount's transactionFees collection.
+    }
 }
--- src/test/java/OnlineAccountTest.java
+++ src/test/java/OnlineAccountTest.java
@@ -2,18 +2,44 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 public class OnlineAccountTest {
 
     OnlineAccount onlineAccount;
+    IChargeable creditCard;
 
     @Before
     public void before() {
         onlineAccount = new OnlineAccount("Polly's Morphin'");
+        IChargeable creditCard = new CreditCard(1234567812345678L, "05/25", 123, "Mastercard", 0.7, 1000.00);
     }
 
     @Test
     public void canGetName() {
         assertEquals("Polly's Morphin'", onlineAccount.getName());
     }
+
+    @Test
+    public void canAddPaymentMethod() {
+        onlineAccount.addPaymentMethod(creditCard);
+        assertEquals(1, onlineAccount.getPaymentMethods().size());
+    }
+
+    @Test
+    public void canAddTransactionFee() {
+        onlineAccount.addTransactionFee(10.00);
+        assertEquals(1,onlineAccount.getTransactionFees().size());
+        assertEquals(10.00, onlineAccount.getTransactionFees().get(0), 0.01);
+    }
+
+//    @Test
+//    public void chargingCustomerLogsTransactionFeeInList() {
+//        onlineAccount.addPaymentMethod(creditCard);
+//        onlineAccount.chargeCustomer(0, 100.00);
+//        assertEquals(1, onlineAccount.getTransactionFees().size());
+//        assertEquals(1.40, onlineAccount.getTransactionFees().get(0), 0.01);
+//    }
+//    Should be able to chargeCustomer(int paymentMethodIndex, double transactionCost) via a selected paymentMethod:
+//    charging that payment method (.charge(cost)) and logging the transaction fee in the OnlineAccount's transactionFees collection.
 }
