c21ab43f3e9c95c398f675a960a2cad655474926
AvianLearning <iaradcliffe@hotmail.com>
2020-11-24 20:15:17 +0000

    Created OnlineAccount class and test file. Passed test to get name from getter method

--- src/main/java/OnlineAccount.java
+++ src/main/java/OnlineAccount.java
@@ -1,2 +1,26 @@
+import java.util.ArrayList;
+
 public class OnlineAccount {
+    private String name;
+    private ArrayList<PaymentCard> paymentMethods;
+    private ArrayList<Double> transactionFees;
+
+    public OnlineAccount(String name) {
+        this.name = name;
+        this.paymentMethods = new ArrayList<>();
+        this.transactionFees = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<PaymentCard> getPaymentMethods() {
+        return paymentMethods;
+    }
+
+    public ArrayList<Double> getTransactionFees() {
+        return transactionFees;
+    }
+
 }
--- src/test/java/OnlineAccountTest.java
+++ src/test/java/OnlineAccountTest.java
@@ -1,2 +1,19 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class OnlineAccountTest {
+
+    OnlineAccount onlineAccount;
+
+    @Before
+    public void before() {
+        onlineAccount = new OnlineAccount("Polly's Morphin'");
+    }
+
+    @Test
+    public void canGetName() {
+        assertEquals("Polly's Morphin'", onlineAccount.getName());
+    }
 }
